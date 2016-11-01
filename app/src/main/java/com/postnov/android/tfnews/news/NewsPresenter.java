ee1308efefc85c675d1e929150bb17f3c0af63dd
Valentin Postnov <valpostnov@gmail.com>
2016-11-02 01:11:24 +0300

    add News ui

---
+++ app/src/main/java/com/postnov/android/tfnews/news/NewsPresenter.java
@@ -0,0 +1,56 @@
+package com.postnov.android.tfnews.news;
+
+import com.postnov.android.tfnews.data.entity.News;
+import com.postnov.android.tfnews.data.source.IDataSource;
+import com.postnov.android.tfnews.news.interfaces.INewsPresenter;
+import com.postnov.android.tfnews.news.interfaces.NewsView;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
+
+/**
+ * Created by platon on 01.11.2016.
+ */
+
+public class NewsPresenter implements INewsPresenter {
+
+    private final CompositeSubscription subscription;
+    private final IDataSource repository;
+    private NewsView newsView;
+
+    public NewsPresenter(IDataSource repository) {
+        subscription = new CompositeSubscription();
+        this.repository = repository;
+    }
+
+    @Override
+    public void bind(NewsView view) {
+        newsView = view;
+    }
+
+    @Override
+    public void unbind() {
+        subscription.clear();
+        newsView = null;
+    }
+
+    @Override
+    public void fetchNews() {
+        subscription.add(repository.getNews()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(onNext, onError));
+    }
+
+    private final Action1<News> onNext = news -> {
+        newsView.showProgressView(false);
+        newsView.showNews(news);
+    };
+
+    private final Action1<Throwable> onError = e -> {
+        newsView.showProgressView(false);
+        newsView.showError(e.getMessage());
+    };
+}
