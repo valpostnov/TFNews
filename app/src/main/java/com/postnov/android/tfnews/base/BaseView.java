cec3edd209dd05ac94ef836ad1c88167dccfedc4
Valentin Postnov <valpostnov@gmail.com>
2016-11-02 01:09:05 +0300

    add Base entities

---
+++ app/src/main/java/com/postnov/android/tfnews/base/BaseView.java
@@ -0,0 +1,10 @@
+package com.postnov.android.tfnews.base;
+
+/**
+ * Created by platon on 01.11.2016.
+ */
+
+public interface BaseView {
+    void showError(String error);
+    void showProgressView(boolean show);
+}
